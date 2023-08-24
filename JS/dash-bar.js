b14bd89df72a27ce5a3e1903b7adfad01a7b9bba
Eslam Elfar <eslamsaber707@gmail.com>
2023-08-24 16:34:06 +0300

    last

---
+++ JS/dash-bar.js
@@ -0,0 +1,9 @@
+let side = document.querySelector('.side-bar')
+
+const openSide = () => {
+    side.classList.add('active-side');
+}
+
+const closeSide = () => {
+    side.classList.remove('active-side');
+}
